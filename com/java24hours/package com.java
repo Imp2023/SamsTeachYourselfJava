db5c89adf28841856490bdc9d35fc2beff6cc78b
Imp2023 <124094650+Imp2023@users.noreply.github.com>
2023-07-17 13:30:28 -0700

    First Java Program

---
+++ com/java24hours/package com.java
@@ -0,0 +1,32 @@
+package com.java24hours;
+
+//the code written on the first line is a package identifier.
+//this is a way of grouping java programs together. The line tells
+//the computer to make com.java24hours the package name of the program
+
+public class Salutation {
+//The line Public Class Salutation means: 
+//Computer, name my class Salutations. 
+
+
+    public static void main(String[] arguments) {
+        String greeting = "Salutation mondo!"; 
+        System.out.println(greeting);  
+
+        //the public static void main string tells the computer that
+        //the main portion part of the program begins here. 
+        //Java programs are organized into different sections,
+        //so there should be a way to identify the part 
+        //of a program that is executed first when the program is run. 
+
+        //the [] are a way to group lines in your program, like
+        // () group words together in a sentence. 
+        //these groupings are called blocks.
+        
+        //The statement on line 14 (String greeting = "Salutation mondo!") tells the computer
+        //to store the "Salutation mondo"  in a variable called greeting. 
+        //In a program you must tell the computer what type of information is being stored. 
+
+
+    }
+}
